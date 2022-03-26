d3de4fc9a95fc600ac2b19003704eb677b8d98f4
sgwlee96 <sgwlee@ucdavis.edu>
2022-03-26 10:31:00 +0900

    Day4

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,24 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("#login-form input");
-
+const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    // const event = loginInput.value;
-    console.log(loginInput.value);
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`; // `${}` -> same as f"aaa{}" in python
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
 }
 
 function handleLinkClick(event){
     event.preventDefault();
-    console.log(event)
+    console.log(event);
+
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+link.addEventListener("click", handleLinkClick);    
