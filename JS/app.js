68c4cac3810d618ea688f382c163bea33e2127e9
sgwlee96 <sgwlee@ucdavis.edu>
2022-03-28 17:31:38 +0900

    Day5

--- JS/app.js
+++ JS/app.js
@@ -3,16 +3,22 @@ const loginInput = loginForm.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
+// 반복사용하는 string은 Uppercase constant 로 저장해놓음
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add("hidden");
     const username = loginInput.value;
-    greeting.innerText = `Hello ${username}`; // `${}` -> same as f"aaa{}" in python
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // Use localStorage API to remember the value (key, value)
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
     
 }
+
+
 
 function handleLinkClick(event){
     event.preventDefault();
@@ -23,5 +29,24 @@ function handleLinkClick(event){
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);    
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);    
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+
+
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the username(greeting)
+    paintGreetings(savedUsername);
+    
+}
