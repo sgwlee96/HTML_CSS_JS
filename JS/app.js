016365995901045b7fdb19d51c6265a98d17244c
sgwlee96 <sgwlee@ucdavis.edu>
2022-03-28 17:42:58 +0900

    Day5

--- JS/app.js
+++ JS/app.js
@@ -10,15 +10,13 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     // Use localStorage API to remember the value (key, value)
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    paintGreetings();
     
 }
-
-
 
 function handleLinkClick(event){
     event.preventDefault();
@@ -29,17 +27,12 @@ function handleLinkClick(event){
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkClick);    
 
-
 function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-
-
 
 if(savedUsername === null){
     // show the form
