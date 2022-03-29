6cf716f63b60974ee91ea41c2712eaaf122b7899
sgwlee96 <sgwlee@ucdavis.edu>
2022-03-29 11:23:43 +0900

    Day6

---
+++ JS/js/greetings.js.js
@@ -0,0 +1,45 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const link = document.querySelector("a");
+
+// 반복사용하는 string은 Uppercase constant 로 저장해놓음
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    // Use localStorage API to remember the value (key, value)
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings();
+    
+}
+
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(event);
+
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);    
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the username(greeting)
+    paintGreetings(savedUsername);
+    
+}
