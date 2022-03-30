f9421b22d948878b6246a37f5aa330dae3470139
sgwlee96 <sgwlee@ucdavis.edu>
2022-03-30 15:34:39 -0700

    Day 8

--- JS/js/todo.js
+++ JS/js/todo.js
@@ -2,11 +2,15 @@ const todoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
 const toDoInput = todoForm.querySelector("input");
 
+function paintToDo(newToDo){
+    console.log("I will paint new todo.")
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    console.log(newToDo);
+    paintToDo(newToDo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
