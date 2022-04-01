35b4829b394e5dbad98670f54df6c125cf2918e9
sgwlee96 <sgwlee@ucdavis.edu>
2022-04-01 16:27:05 +0900

    Day 8

--- JS/js/todo.js
+++ JS/js/todo.js
@@ -2,6 +2,14 @@ const todoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
 const toDoInput = todoForm.querySelector("input");
 
+const toDos = [];
+
+const TODOS_KEY = "toDos";
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDO(event){
     const li = event.target.parentElement;
     li.remove();
@@ -12,7 +20,7 @@ function paintToDo(newToDo){
     const span = document.createElement("span");
     span.innerText = newToDo;
     const button = document.createElement("button");
-    button.innerText = "❌"
+    button.innerText = "❌";
     button.addEventListener("click", deleteToDO);
 
     // append should be added at the last
@@ -25,7 +33,17 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    console.log(parsedToDos);
+    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+}
